88c333a71e2fd09977af2e1abd67a560bf92df1e
Z-e-Games <93876011+Z-e-Games@users.noreply.github.com>
2022-02-13 21:52:15 -0600

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
-c.beginPath()
-c.arc(canvas.width/2,canvas.height/2,200,Math.PI * 2,false)
-c.fillStyle = 'blue'
-c.fill()
